80d5a6bc6403f017f4161cc26d18b94197091ef2
Michael Speiner <speiner.m@gmail.com>
2023-10-31 12:30:12 +0100

    Skapar upp paneler i Huvudkoden

--- src/Huvudkod.java
+++ src/Huvudkod.java
@@ -1,3 +1,9 @@
-public class Huvudkod {
+import javax.swing.*;
 
+public class Huvudkod {
+    //Skapar upp paneler Huvudkod
+    final static JPanel jp = new JPanel();
+    final static JPanel northPanel = new JPanel();
+    final static JPanel centerPanel = new JPanel();
+    final static JPanel southPanel = new JPanel();
 }
